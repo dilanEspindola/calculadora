2e77163ddfc7a03125d62cc37c8730201fc45717
dilan Espindola <dilanespindola30@gmail.com>
2022-04-07 23:26:03 -0500

    proyecto terminado

--- scripts/changeTheme.js
+++ scripts/changeTheme.js
@@ -1,23 +1,21 @@
-var _a, _b, _c;
-window.addEventListener("DOMContentLoaded", function () {
-    var _a;
-    (_a = document.querySelector('body')) === null || _a === void 0 ? void 0 : _a.classList.add("active-1");
+window.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("body").classList.add("active-1");
 });
-(_a = document.querySelector(".uno")) === null || _a === void 0 ? void 0 : _a.addEventListener('click', function () {
-    var _a, _b, _c;
-    (_a = document.querySelector('body')) === null || _a === void 0 ? void 0 : _a.classList.add("active-1");
-    (_b = document.querySelector('body')) === null || _b === void 0 ? void 0 : _b.classList.remove("active-2");
-    (_c = document.querySelector('body')) === null || _c === void 0 ? void 0 : _c.classList.remove("active-3");
+
+document.querySelector(".uno").addEventListener("click", () => {
+  document.querySelector("body").classList.add("active-1");
+  document.querySelector("body").classList.remove("active-2");
+  document.querySelector("body").classList.remove("active-3");
 });
-(_b = document.querySelector(".dos")) === null || _b === void 0 ? void 0 : _b.addEventListener('click', function () {
-    var _a, _b, _c;
-    (_a = document.querySelector('body')) === null || _a === void 0 ? void 0 : _a.classList.add("active-2");
-    (_b = document.querySelector('body')) === null || _b === void 0 ? void 0 : _b.classList.remove("active-3");
-    (_c = document.querySelector('body')) === null || _c === void 0 ? void 0 : _c.classList.remove("active-1");
+
+document.querySelector(".dos").addEventListener("click", () => {
+  document.querySelector("body").classList.add("active-2");
+  document.querySelector("body").classList.remove("active-3");
+  document.querySelector("body").classList.remove("active-1");
 });
-(_c = document.querySelector(".tres")) === null || _c === void 0 ? void 0 : _c.addEventListener('click', function () {
-    var _a, _b, _c;
-    (_a = document.querySelector('body')) === null || _a === void 0 ? void 0 : _a.classList.add("active-3");
-    (_b = document.querySelector('body')) === null || _b === void 0 ? void 0 : _b.classList.remove("active-1");
-    (_c = document.querySelector('body')) === null || _c === void 0 ? void 0 : _c.classList.remove("active-2");
+
+document.querySelector(".tres").addEventListener("click", () => {
+  document.querySelector("body").classList.add("active-3");
+  document.querySelector("body").classList.remove("active-1");
+  document.querySelector("body").classList.remove("active-2");
 });
--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,11 @@
 function reset() {
-    var _a;
-    (_a = document.querySelector("form")) === null || _a === void 0 ? void 0 : _a.reset();
+  document.querySelector("form").reset();
 }
+
 function borrar() {
-    var pantallaInput = document.getElementById("pantalla");
-    document.getElementById('pantalla').value = pantallaInput.slice(0, pantallaInput.length - 1);
+  const pantallaInput = document.getElementById("pantalla").value;
+  document.getElementById("pantalla").value = pantallaInput.slice(
+    0,
+    pantallaInput.length - 1
+  );
 }
